92865d292c730f2e3cce50f89118cc70b4322afa
becastro <benatcastro@gmail.com>
2023-11-09 11:56:08 +0100

    Requests to the open Ai working correctly.

--- src/main/java/aiintegration/api/APIClient.java
+++ src/main/java/aiintegration/api/APIClient.java
@@ -1,5 +1,13 @@
 package aiintegration.api;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 import javax.json.*;
 import java.io.*;
 import java.net.*;
@@ -22,71 +30,73 @@ public class APIClient {
         }
     }
 
-    private void requestBodyBuilder() {
+    private String requestBodyBuilder() {
 
-        final String model = "gpt-3.5-turbo";
+        final String model = "gpt-3.5-turbo-1106";
         final String prompt = "say test";
 
         JsonArrayBuilder    messageBuilder = Json.createArrayBuilder();
         JsonObjectBuilder   bodyBuilder = Json.createObjectBuilder();
 
+        JsonObject systemMessage = Json.createObjectBuilder()
+                .add("role", "system")
+                .add("content", "you are an insurance expert with the task to analyze this data and provide a comprehensive risk evaluation on a scale of 0 to 100. The output should include a detailed JSON report outlining the factors contributing to the risk score, offering a clear and concise summary of the identified risks")
+                .build();
+
         JsonObject userMessage = Json.createObjectBuilder()
                 .add("role","user")
-                .add("content", prompt)
+                .add("content", this.data.toString())
                 .build();
-        messageBuilder.add(userMessage);
+
+        JsonObject formatType = Json.createObjectBuilder()
+                        .add("type", "json_object")
+                        .build();
+
+        messageBuilder.add(systemMessage)
+                        .add(userMessage);
 
         bodyBuilder.add("model", model)
+                .add("response_format", formatType)
                 .add("messages", messageBuilder.build());
 
-        System.out.print("Mybody ->");
-        System.out.println(bodyBuilder.build().toString());
-//        return jsonArrayBuilder.build();
+        return bodyBuilder.build().toString();
 
 
     }
 
-    public void makeApiRequest() {
+    public void makeApiRequest() throws IOException {
 
         String url = "https://api.openai.com/v1/chat/completions";
         String apiKey = "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";
-        String model = "gpt-3.5-turbo";
 
+        HttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url);
+
+        // Adding neccesary http headers for the request
+        httpPost.setHeader("Authorization", "Bearer " + apiKey);
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-Type", "application/json");
+
+        // Adding the prompt information to the request as the body
+        StringEntity body_entity;
+        String body_data = this.requestBodyBuilder();
+        System.out.println("Body->" + body_data);
         try {
-            URL obj = new URL(url);
-            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Authorization", "Bearer " + apiKey);
-            connection.setRequestProperty("Content-Type", "application/json");
+            body_entity = new StringEntity(body_data);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        httpPost.setEntity(body_entity);
 
-            // The request body
-            this.requestBodyBuilder();
-            String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + "say test" + "\"}]}";
-
-            System.out.println("Body->" + body);
-            connection.setDoOutput(true);
-            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
-            writer.write(body);
-            writer.flush();
-            writer.close();
-
-            // Response from ChatGPT
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-
-            StringBuffer response = new StringBuffer();
-
-            while ((line = br.readLine()) != null) {
-                response.append(line);
-            }
-            br.close();
-
-            // calls the method to extract the message.
-            System.out.println(response);
-
+        // Executing request
+        try {
+            HttpResponse response = client.execute(httpPost);
+            String responseBody = EntityUtils.toString(response.getEntity());
+            System.out.println("Response body- " + responseBody);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 
 }
