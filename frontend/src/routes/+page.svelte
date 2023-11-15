ac129e52e69f18a0754999a89ad9787b712eb750
Your Name <you@example.com>
2023-11-15 02:31:27 +0100

    accordion work

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {Accordion, AccordionItem, Progress} from "sveltestrap";
+
+    let data = [
+        {id: 0, score: 1, summary: "lorem ipsum"},
+        {id: 1, score: 20, summary: "lorem ipsum"},
+        {id: 2, score: 15, summary: "lorem ipsum"},
+        {id: 3, score: 1, summary: "lorem ipsum"},
+        {id: 4, score: 60, summary: "lorem ipsum"},
+        {id: 5, score: 80, summary: "lorem ipsum"},
+        {id: 6, score: 90, summary: "lorem ipsum"},
+        {id: 7, score: 5, summary: "lorem ipsum"},
+        {id: 8, score: 17, summary: "lorem ipsum"},
+        {id: 9, score: 19, summary: "lorem ipsum"},
+        {id: 10, score: 23, summary: "lorem ipsum"},
+    ]
+
+    function calculateScoreBarColor(score) {
+        switch (Number(score)) {
+
+            case score > 75:
+                return "success"
+            case score > 25:
+                return "warning"
+            case score > 0:
+                return "danger"
+        }
+    }
+
+
+</script>
+
+<Accordion>
+    {#each data as entry}
+        <AccordionItem>
+            <div slot="header" class="accordion-header" style="width: 50%;" >
+                <h4 >Entry id: {entry.id}</h4>
+                <div style="">Score</div>
+                <Progress value={entry.score} color="{calculateScoreBarColor(entry.score)}"/>
+            </div>
+            {entry.score}
+            </AccordionItem>
+    {/each}
+
+</Accordion>
+
+<style>
+</style>
