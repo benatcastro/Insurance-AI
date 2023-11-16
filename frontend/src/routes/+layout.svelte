797f1550bcede837b3bde22842501364e81a0669
Your Name <you@example.com>
2023-11-16 18:54:09 +0100

    how to use and minor fixes

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import {Nav, Navbar, NavbarBrand, NavItem, NavLink} from "sveltestrap";
+</script>
+<Navbar color="light">
+    <NavbarBrand>AI Risk Handler</NavbarBrand>
+</Navbar>
+<slot></slot>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,39 +1,77 @@
 <script>
-    import { Button,  Form, FormGroup, Input, Label } from "sveltestrap";
+    import {
+        Alert,
+        Button,
+        Form,
+        FormGroup,
+        Input,
+        Label,
+        Spinner,
+    } from "sveltestrap";
     import DataShowcase from "./DataShowcase.svelte";
 
-    let numberOfEntries = 0;
-    let startFrom = 0
-    let sourceFile = "insurance.csv"
-    let data;
+    let numberOfEntries = 5;
+    let sourceFile = "medical.csv"
+    $: responseData = null;
 
-    async function getEntries() {
-        const url = new URL("http://localhost:4567");
+    const url = new URL('http://localhost:4567');
 
-        console.log("Starting to fetch...");
-        data = "loading"
-        const response = await fetch(url);
+    let howToUseModelOpen = false;
 
-        data = await response.json()
-        console.log("response:" + data);
-    }
+    const toggleHowToUseModal = () => (howToUseModelOpen = !howToUseModelOpen);
+
+    const fetchData = async () => {
+
+        const params = new URLSearchParams();
+
+        params.set("entries", numberOfEntries);
+
+        if (sourceFile === "Medical")
+            params.set("file", "medical.csv");
+        else
+            params.set("file", "mental-health.csv");
+
+
+        const endpoint = url + "?" + params;
+
+        console.log("starting fecth to ", endpoint);
+        try {
+            responseData = "loading";
+            const response = await fetch(endpoint);
+            responseData = await response.json();
+            console.log("Fetch finished: ", responseData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            responseData = "error";
+        }
+    };
+
 </script>
 
 <main>
-    <h3>Request Input configurator</h3>
-    <div class="input-container">
+    <div class="introduction">
+        <Alert color="light" dismissible heading="Welcome to my demo!">
+            <p>When you click on "get results", a request with the selected parameters will be send to the backend</p>
+            <p>The backend will parse the input file and use OpenAI api to analyze it, this may take some minutes since it is dependant on external services</p>
+            <b>How to use</b>
+            <p>Choose the parameters, click "get results" and wait.</p>
+            <p><b>Dataset</b>: the dataset that will be analyzed.</p>
+            <p><b>Entries</b>: how many of the entries in the dataset will be anylized.</p>
 
+        </Alert>
+
+    </div>
+    <div class="input-container">
         <Form>
             <FormGroup>
-                <Label for="Source">Source</Label>
+                <Label for="Dataset">Dataset</Label>
                 <Input
                         type="select"
                         name="Some text value"
                         bind:value={sourceFile}
                 >
-                    <option>File one</option>
-                    <option>File two</option>
-                    <option>File three</option>
+                    <option>Medical</option>
+                    <option>Mental Health</option>
                 </Input>
 
             </FormGroup>
@@ -45,7 +83,7 @@
                         type="range"
                         name="range"
                         id="exampleRange"
-                        min={0}
+                        min={5}
                         max={100}
                         step={5}
                         placeholder="Range placeholder"
@@ -54,53 +92,50 @@
 
             </FormGroup>
         </Form>
-        <Form>
-            <FormGroup>
-                <Label for="">Start from</Label>
-                <Input
-                        type="number"
-                        name="number"
-                        id="startingFromInput"
-                        placeholder="Select where you want to start from"
-                        bind:value={startFrom}
-                />
-            </FormGroup>
-        </Form>
-        <div class="buttons">
-            <Button color="primary" on:click={getEntries()}>Get results</Button>
-            <Button color="primary">How to use</Button>
-            <Button color="primary">More Info</Button>
-        </div>
+        <Button color="primary"  on:click={fetchData}>Get results</Button>
     </div>
     <div class="data-container">
-        <DataShowcase {data} ></DataShowcase>
+        {#if (responseData == null)}
+            <div class="status-tag">
+                <h6>Waiting for request...</h6>
+            </div>
+        {:else if (responseData === "loading")}
+            <div class="status-tag">
+                <p>Loading results</p>
+                <Spinner></Spinner>
+            </div>
+        {:else if (responseData === "error")}
+            <div class="status-tag">
+                <p>Error loading results, please try again</p>
+            </div>
+        {:else}
+            <DataShowcase {responseData} ></DataShowcase>
+        {/if}
 
     </div>
 
+    <h6 style="text-align: center">Made by Beñat Castro</h6>
 
 </main>
 <style>
+
     main {
         margin-top: 2rem;
-        gap: 2rem;
+        gap: 0.5rem;
         display: flex;
         align-items: center;
         flex-direction: column;
-        height: 100vh;
+        height: 90vh;
         width: 100vw;
     }
     h3 {
-        margin-bottom: 5rem;
-        text-align: center;
+        margin-bottom: 2rem;
+    }
+    .introduction {
+        width: 80%;
     }
 
-    .buttons {
-        width: 100%;
-        height: auto;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+
     .input-container {
         justify-content: center;
         align-items: center;
@@ -115,8 +150,15 @@
     .data-container {
         width: 80%;
     }
+
+    .status-tag {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+    }
     @media (max-width: 900px) {
-        .input-container, .buttons {
+        .input-container {
             grid-template-columns: 1fr;
         }
 
